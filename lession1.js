c0ef67cb9d7bf81b03c6facf34073183139336fe
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-07-07 14:12:34 +0530

    added lession1

---
+++ lession1.js
@@ -0,0 +1,24 @@
+function double(number) {
+    return number * 2;
+}
+
+console.log(double(2));
+console.log(double(4));
+
+function triple(number) {
+    return number * 3;
+}
+
+// addition function
+function add(a, b) {
+    return a + b
+}
+// sum function
+function sum(x, y, z) {
+    return x + y + z
+}
+console.log(triple(4));
+console.log(triple(2));
+
+console.log(add(2,5)); // will return 7
+console.log(sum(2,5,2)); // will return 9
