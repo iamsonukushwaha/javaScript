e2d4f9e2ea709e72701cc15d84ece4072f97c05c
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2023-10-01 12:31:24 +0530

    Create search.js

---
+++ search.js
@@ -0,0 +1,9 @@
+const planet = "Earth";
+console.log(planet);
+console.log(planet.startsWith("E"));
+console.log(planet.startsWith("e"));
+console.log(planet.endsWith("th"));
+console.log(planet.includes("art"));
+console.log(planet.includes("Earth1"));
+const planet2 = "Marsaerst";
+console.log(planet2.search("rs"));
